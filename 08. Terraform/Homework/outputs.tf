733f9c9191c214f51e15864add92ecdfc112c813
alchemija <96947939+alchemija@users.noreply.github.com>
2025-01-30 14:54:17 +0200

    removed old folders

--- 08. Terraform/Homework/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ips" {
-  description = "Public IPs of the EC2 instances"
-  value       = aws_instance.web[*].public_ip
-}
-
-output "apache_urls" {
-  description = "URLs of the Apache web servers"
-  value       = [for ip in aws_instance.web[*].public_ip : "http://${ip}"]
-}
